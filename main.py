c7a83c2d682beb758c0d343979e72beff728b784
Fatimat Atanda <atandafatimat01@gmail.com>
2025-10-22 16:16:43 -0500

    heroku buildpack

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from ml.data import process_data
 from ml.model import inference
 
 # DVC setup for Heroku
-if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+# if "DYNO" in os.environ and os.path.isdir(".dvc"):
+#     os.system("dvc config core.no_scm true")
+#     if os.system("dvc pull") != 0:
+#         exit("dvc pull failed")
+#     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 # Initialize FastAPI app
 app = FastAPI()
